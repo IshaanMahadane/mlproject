55efb44cf447349cf15efa5c62a7c3babc6639c2
IshaanMahadane <97539146+IshaanMahadane@users.noreply.github.com>
2025-10-29 18:49:23 +0530

    EDA and Model Builing

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,13 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name = exc_tb
     error_message = "Error occured in python script name[{0}] line number [{1}] errror message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error)
+     file_name,exc_tb.tb_lineno,str(error))
     return error_message
-    )
+    
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
